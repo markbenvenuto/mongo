b73cc7e8fc766abdd7d7bfa564b8a9775fa3aa91
Mark Benvenuto <mark.benvenuto@mongodb.com>
2017-03-01 16:45:12 -0500

    stub

---
+++ src/mongo/idl/idl_options.h
@@ -0,0 +1,33 @@
+/*
+ *    Copyright (C) 2016 MongoDB Inc.
+ */
+
+#pragma once
+
+#include "mongo/base/status.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace mongo {
+
+namespace optionenvironment {
+class OptionSection;
+class Environment;
+}  // namespace optionenvironment
+
+namespace moe = mongo::optionenvironment;
+
+struct IDLToolOptions {
+    bool color;
+    std::string user;
+};
+
+extern IDLToolOptions* globalIDLToolOptions;
+
+Status addIDLToolOptions(moe::OptionSection* options);
+
+Status storeIDLToolOptions(const moe::Environment& params, const std::vector<std::string>& args);
+
+}  // mongo
