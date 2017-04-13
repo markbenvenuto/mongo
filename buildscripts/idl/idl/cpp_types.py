bafda133d600e94143abf314de9d77eb69120d89
Mark Benvenuto <mark.benvenuto@mongodb.com>
2017-04-13 14:44:19 -0400

    refactor

---
+++ buildscripts/idl/idl/cpp_types.py
@@ -0,0 +1,207 @@
+# Copyright (C) 2017 MongoDB Inc.
+#
+# This program is free software: you can redistribute it and/or  modify
+# it under the terms of the GNU Affero General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""IDL C++ Code Generator."""
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+import io
+import os
+import string
+import sys
+import textwrap
+from typing import List, Mapping, Union
+
+from . import ast
+from . import bson
+from . import common
+
+
+
+def _is_primitive_type(cpp_type):
+    # type: (unicode) -> bool
+    """Return True if a cpp_type is a primitive type and should not be returned as reference."""
+    return cpp_type in [
+        'bool', 'double', 'std::int32_t', 'std::uint32_t', 'std::uint64_t', 'std::int64_t'
+    ]
+
+
+def _is_view_type(cpp_type):
+    # type: (unicode) -> bool
+    """Return True if a cpp_type should be returned as a view type from an IDL class."""
+    if cpp_type == 'std::string':
+        return True
+
+    if cpp_type == 'std::vector<std::uint8_t>':
+        return True
+
+    return False
+
+
+def _get_view_type(cpp_type):
+    # type: (unicode) -> unicode
+    """Map a C++ type to its C++ view type if needed."""
+    if cpp_type == 'std::string':
+        cpp_type = 'StringData'
+
+    if cpp_type == 'std::vector<std::uint8_t>':
+        cpp_type = 'mongo::ConstDataRange'
+
+    return cpp_type
+
+
+def _get_view_type_to_base_method(cpp_type):
+    # type: (unicode) -> unicode
+    """Map a C++ View type to its C++ base type."""
+    assert _is_view_type(cpp_type)
+
+    if cpp_type == 'std::vector<std::uint8_t>':
+        return "True"
+
+    return "toString"
+
+
+def _get_field_cpp_type(field):
+    # type: (ast.Field) -> unicode
+    """Get the C++ type name for a field."""
+    assert field.cpp_type is not None or field.struct_type is not None
+
+    if field.struct_type:
+        cpp_type = common.title_case(field.struct_type)
+    else:
+        cpp_type = field.cpp_type
+
+    return cpp_type
+
+
+def _qualify_optional_type(cpp_type, field):
+    # type: (unicode, ast.Field) -> unicode
+    """Qualify the type if the field is optional."""
+    if field.optional:
+        return 'boost::optional<%s>' % (cpp_type)
+
+    return cpp_type
+
+
+def _qualify_array_type(cpp_type, field):
+    # type: (unicode, ast.Field) -> unicode
+    """Qualify the type if the field is an array."""
+    if field.array:
+        cpp_type = "std::vector<%s>" % (cpp_type)
+
+    return cpp_type
+
+
+def _get_field_getter_setter_type(field):
+    # type: (ast.Field) -> unicode
+    """Get the C++ type name for the getter/setter parameter for a field."""
+    assert field.cpp_type is not None or field.struct_type is not None
+
+    cpp_type = _get_field_cpp_type(field)
+
+    cpp_type = _get_view_type(cpp_type)
+
+    cpp_type = _qualify_array_type(cpp_type, field)
+
+    return _qualify_optional_type(cpp_type, field)
+
+
+def _get_field_storage_type(field):
+    # type: (ast.Field) -> unicode
+    """Get the C++ type name for the storage of class member for a field."""
+    cpp_type = _get_field_cpp_type(field)
+
+    cpp_type = _qualify_array_type(cpp_type, field)
+
+    return _qualify_optional_type(cpp_type, field)
+
+
+def _get_return_by_reference(field):
+    # type: (ast.Field) -> bool
+    """Return True if the type should be returned by reference."""
+    # For non-view types, return a reference for types:
+    #  1. arrays
+    #  2. nested structs
+    # But do not return a reference for:
+    #  1. std::int32_t and other primitive types
+    #  2. optional types
+    cpp_type = _get_field_cpp_type(field)
+
+    if not _is_view_type(cpp_type) and (not field.optional and
+                                        (not _is_primitive_type(cpp_type) or field.array)):
+        return True
+
+    return False
+
+
+def _get_disable_xvalue(field):
+    # type: (ast.Field) -> bool
+    """Return True if the type should have the xvalue getter disabled."""
+    # Any we return references or view types, we should disable the xvalue.
+    # For view types like StringData, the return type and member storage types are different
+    # so returning a reference is not supported.
+    cpp_type = _get_field_cpp_type(field)
+
+    return _is_view_type(cpp_type) or _get_return_by_reference(field)
+
+
+
+
+class CppTypeBase(object):
+    """Base type for C++ Type information."""
+
+    def __init__(self, field):
+        # type: (ast.Field) -> None
+        self._field = field
+
+    def get_type_name(self):
+        # type: () -> unicode
+        return _get_field_cpp_type(self._field)
+
+    def get_storage_type(self):
+        # type: () -> unicode
+        # _get_field_storage_type
+        return _get_field_storage_type(self._field)
+
+    def get_getter_setter_type(self):
+        # type: () -> unicode
+        # _get_field_getter_setter_type
+        return _get_field_getter_setter_type(self._field)
+
+    def return_by_reference(self):
+        # type: () -> bool
+        # _get_return_by_reference
+        return _get_return_by_reference(self._field)
+
+    def disable_xvalue(self):
+        # type: () -> bool
+        # _get_disable_xvalue
+        return _get_disable_xvalue(self._field)
+
+    def is_view_type(self):
+        # type: () -> bool
+        # _is_view_type
+        return _is_view_type(self.get_type_name())
+    
+    def get_view_type_to_base_method(self):
+        # type: () -> unicode
+        # _get_view_type_to_base_method
+        return _get_view_type_to_base_method(self.get_type_name())
+
+
+def get_cpp_type(field):
+    # type: (ast.Field) -> CppTypeBase
+    """Get the C++ Type information for the given field."""
+
+    return CppTypeBase(field)
