86e00faf83dc4bf2ff6faf52463ddf7e1054223f
Mark Benvenuto <mark.benvenuto@mongodb.com>
2019-10-15 11:57:06 -0400

    quick

---
+++ buildscripts/quickcpplint.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+"""Extensible script to run one or more Simple C++ Linters across a subset of files in parallel."""
+
+import argparse
+import logging
+import os
+import re
+import sys
+import threading
+from abc import ABCMeta, abstractmethod
+from typing import Any, Dict, List
+
+# Get relative imports to work when the package is not installed on the PYTHONPATH.
+if __name__ == "__main__" and __package__ is None:
+    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(os.path.realpath(__file__)))))
+
+from buildscripts.linter import base  # pylint: disable=wrong-import-position
+from buildscripts.linter import git  # pylint: disable=wrong-import-position
+from buildscripts.linter import parallel  # pylint: disable=wrong-import-position
+from buildscripts.linter import runner  # pylint: disable=wrong-import-position
+from buildscripts.linter import simplecpplinter  # pylint: disable=wrong-import-position
+from buildscripts.linter import simplecpplint  # pylint: disable=wrong-import-position
+
+FILES_RE = re.compile('\\.(h|cpp)$')
+
+def is_interesting_file(file_name):
+    # type: (str) -> bool
+    """Return true if this file should be checked."""
+    return (file_name.startswith("jstests")
+            or file_name.startswith("src") and not file_name.startswith("src/third_party/")
+            and not file_name.startswith("src/mongo/gotools/")) and FILES_RE.search(file_name)
+
+
+def _lint_files(file_names):
+    # type: (str, Dict[str, str], List[str]) -> None
+    """Lint a list of files with clang-format."""
+    lint_runner = runner.LintRunner()
+
+    linter_instances = [base.LinterInstance(simplecpplinter.SimpleCppLinter(), [sys.executable])]
+
+    failed_lint = False
+
+    for linter in linter_instances:
+        # run_lint1 = lambda param1: lint_runner.run_lint(linter, param1)  # pylint: disable=cell-var-from-loop
+        # lint_clean = parallel.parallel_process([os.path.abspath(f) for f in file_names], run_lint1)
+        run_lint2 = lambda param2 : simplecpplint.lint_file(param2) == 0
+        lint_clean = parallel.parallel_process([os.path.abspath(f) for f in file_names], run_lint2)
+
+        if not lint_clean:
+            failed_lint = True
+
+    if failed_lint:
+        print("ERROR: Code Style does not match coding style")
+        sys.exit(1)
+
+
+def lint_patch(file_name):
+    # type: (str, Dict[str, str], List[str]) -> None
+    """Lint patch command entry point."""
+    file_names = git.get_files_to_check_from_patch(file_name, is_interesting_file)
+
+    # Patch may have files that we do not want to check which is fine
+    if file_names:
+        _lint_files(file_names)
+
+
+def lint(file_names):
+    # type: (str, Dict[str, str], List[str]) -> None
+    """Lint files command entry point."""
+    all_file_names = git.get_files_to_check(file_names, is_interesting_file)
+
+    _lint_files(all_file_names)
+
+
+def lint_all(file_names):
+    # type: (str, Dict[str, str], List[str]) -> None
+    # pylint: disable=unused-argument
+    """Lint files command entry point based on working tree."""
+    all_file_names = git.get_files_to_check_working_tree(is_interesting_file)
+
+    _lint_files(all_file_names)
+
+
+def main():
+    # type: () -> None
+    """Execute Main entry point."""
+
+    parser = argparse.ArgumentParser(description='Quick C++ Lint frontend.')
+
+    parser.add_argument('-v', "--verbose", action='store_true', help="Enable verbose logging")
+
+    sub = parser.add_subparsers(title="Linter subcommands", help="sub-command help")
+
+    parser_lint = sub.add_parser('lint', help='Lint only Git files')
+    parser_lint.add_argument("file_names", nargs="*", help="Globs of files to check")
+    parser_lint.set_defaults(func=lint)
+
+    parser_lint_all = sub.add_parser('lint-all', help='Lint All files')
+    parser_lint_all.add_argument("file_names", nargs="*", help="Globs of files to check")
+    parser_lint_all.set_defaults(func=lint_all)
+
+    parser_lint_patch = sub.add_parser('lint-patch', help='Lint the files in a patch')
+    parser_lint_patch.add_argument("file_names", nargs="*", help="Globs of files to check")
+    parser_lint_patch.set_defaults(func=lint_patch)
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+
+    args.func(args.file_names)
+
+
+if __name__ == "__main__":
+    main()
