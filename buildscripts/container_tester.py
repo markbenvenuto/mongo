03cb1f6ada58d0daae04cbef701f3e7f10743038
Mark Benvenuto <mark.benvenuto@mongodb.com>
2019-12-12 16:37:41 -0500

    snap

--- buildscripts/container_tester.py
+++ buildscripts/container_tester.py
@@ -7,14 +7,15 @@ Assumes remote host is Linux
 """
 
 import argparse
+import datetime
+import json
 import logging
 import os
-import re
-import sys
-import json
-import threading
-import subprocess
 import pprint
+import re
+import subprocess
+import sys
+import threading
 from typing import List
 
 import boto3
@@ -34,13 +35,13 @@ LOGGER = logging.getLogger(__name__)
 #   3 - Automate teardown of Podman
 
 # TODO
-# 1. Add tags to services/tags
 # 2. Add way to garbage collect all tasks older then X?
 #    - May not need tags if all confined to one cluster
 
 ############################################################################
 # Local configuration settings for testing against the local docker/podman
 #
+# TODO - in order to finish the ECS emulation, we have to tweak the networking of the container
 DOCKER_EXECUTABLE = "podman"
 
 IMAGE_NAME = "fedora_sshd"
@@ -57,7 +58,7 @@ CONTAINER_DEFAULT_NAME = "container_tester_test1"
 
 # These settings depend on a cluster, task subnet, and security group already setup
 ECS_DEFAULT_CLUSTER = "arn:aws:ecs:us-east-2:579766882180:cluster/tf-mcb-ecs-cluster"
-ECS_DEFAULT_TASK_DEFINITION = "arn:aws:ecs:us-east-2:579766882180:task-definition/tf-app:1"
+ECS_DEFAULT_TASK_DEFINITION = "arn:aws:ecs:us-east-2:579766882180:task-definition/tf-app:2"
 ECS_DEFAULT_SUBNET = 'subnet-a5e114cc'
 # Must allow ssh from 0.0.0.0
 ECS_DEFAULT_SECURITY_GROUP = 'sg-051a91d96332f8f3a'
@@ -65,53 +66,16 @@ ECS_DEFAULT_SECURITY_GROUP = 'sg-051a91d96332f8f3a'
 # This is just a string local to this file
 DEFAULT_SERVICE_NAME = 'script-test2'
 
+# Garbage collection threshold for old/stale services
+DEFAULT_GARBAGE_COLLECTION_THRESHOLD = datetime.timedelta(hours=1)
+
 ############################################################################
 
 
-class GitCommandResult(object):
-    """The result of running git subcommand.
-
-    Args:
-        cmd: the git subcommand that was executed (e.g. 'clone', 'diff').
-        process_args: the full list of process arguments, starting with the 'git' command.
-        returncode: the return code.
-        stdout: the output of the command.
-        stderr: the error output of the command.
-    """
-
-    def __init__(  # pylint: disable=too-many-arguments
-            self, process_args, returncode, stdout=None, stderr=None):
-        """Initialize GitCommandResult."""
-        self.process_args = process_args
-        self.returncode = returncode
-        self.stdout = stdout
-        self.stderr = stderr
-
-    def check_returncode(self):
-        """Raise GitException if the exit code is non-zero."""
-        if self.returncode:
-            raise GitException(
-                "Process failed with code '{0}'".format(" ".join(self.process_args),
-                                                              self.returncode), self.returncode,
-                self.process_args, self.stdout, self.stderr)
-
-
-# def _run_cmd(self, cmd, args):
-#     """Run the git command and return a GitCommandResult instance."""
-
-#     params = ["git", cmd] + args
-#     return _run_process(cmd, params)
-
 def _run_process(params, cwd=None):
     LOGGER.info("RUNNING COMMAND: %s", params)
-    process = subprocess.Popen(params, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd)
-    (stdout, stderr) = process.communicate()
-    if process.returncode:
-        if stdout:
-            LOGGER.error("Output of '%s': %s", " ".join(params), stdout)
-        if stderr:
-            LOGGER.error("Error output of '%s': %s", " ".join(params), stderr)
-    return GitCommandResult(params, process.returncode, stdout=stdout, stderr=stderr)
+    ret = subprocess.run(params, cwd=cwd)
+    return ret.returncode
 
 def _userandhostandport(endpoint):
     user_and_host = endpoint.find("@")
@@ -133,14 +97,21 @@ def _ssh(endpoint, cmd):
     (user, host, port) = _userandhostandport(endpoint)
     cmd = ["ssh", "-o", "StrictHostKeyChecking=no", "-p", port, "%s@%s" % (user, host), cmd ]
     ret = _run_process(cmd)
-    print(ret.stdout.decode('utf-8'))
-    LOGGER.info("RETURN CODE: %s", ret.returncode)
-    return ret.returncode
+    LOGGER.info("RETURN CODE: %s", ret)
+    return ret
 
 def _run_test_args(args):
     run_test(args.endpoint, args.script, args.files)
 
 def run_test(endpoint, script, files):
+    """
+    Run a test on a machine
+
+    Steps
+    1. Copy over a files which are tuples of (src, dest)
+    2. Copy over the test script to "/tmp/test.sh"
+    3. Run the test script and return the results
+    """
     LOGGER.info("Copying files to %s", endpoint)
 
     for file in files:
@@ -156,24 +127,25 @@ def run_test(endpoint, script, files):
         raise ValueError(f"test failed with {return_code}")
 
 def _local_start_container_args(args):
-#    start_container_args(args)
     port_mapping = f"127.0.0.1:{CONTAINER_DEFAULT_PORT}:22"
-    ret = _run_process([DOCKER_EXECUTABLE, "run", "-p", port_mapping, "--name", CONTAINER_DEFAULT_NAME, "--rm", "-d", IMAGE_NAME ])
-    print(ret.stdout.decode('utf-8'))
+    _run_process([DOCKER_EXECUTABLE, "run", "-p", port_mapping, "--name", CONTAINER_DEFAULT_NAME, "--rm", "-d", IMAGE_NAME ])
 
 def _local_stop_container_args(args):
-#    start_container_args(args)
-    ret = _run_process([DOCKER_EXECUTABLE, "stop", CONTAINER_DEFAULT_NAME])
-    print(ret.stdout.decode('utf-8'))
+     _run_process([DOCKER_EXECUTABLE, "stop", CONTAINER_DEFAULT_NAME])
 
 def _get_region(arn):
     return arn.split(':')[3]
 
 
 def _remote_ps_container_args(args):
-    _remote_ps_container(ECS_DEFAULT_CLUSTER)
+    remote_ps_container(ECS_DEFAULT_CLUSTER)
 
-def _remote_ps_container(cluster):
+def remote_ps_container(cluster):
+    """
+    Get a list of task running in the cluster with their network addresses.
+
+    Emulates the docker ps and ecs-cli ps commands.
+    """
     ecs_client = boto3.client('ecs', region_name=_get_region(cluster))
     ec2_client = boto3.client('ec2', region_name=_get_region(cluster))
 
@@ -213,9 +185,12 @@ def _remote_ps_container(cluster):
         print("{:<43}{:<9}{:<25}{:<16}".format(task_id, lastStatus, public_ip, taskDefinition_short ))
 
 def _remote_create_container_args(args):
-    _remote_create_container(ECS_DEFAULT_CLUSTER, ECS_DEFAULT_TASK_DEFINITION, DEFAULT_SERVICE_NAME, ECS_DEFAULT_SUBNET, ECS_DEFAULT_SECURITY_GROUP)
+    remote_create_container(ECS_DEFAULT_CLUSTER, ECS_DEFAULT_TASK_DEFINITION, DEFAULT_SERVICE_NAME, ECS_DEFAULT_SUBNET, ECS_DEFAULT_SECURITY_GROUP)
 
-def _remote_create_container(cluster, task_definition, service_name, subnet, security_group):
+def remote_create_container(cluster, task_definition, service_name, subnet, security_group):
+    """
+    Create a task in ECS
+    """
     ecs_client = boto3.client('ecs', region_name=_get_region(cluster))
 
     # TODO - consider clientToken
@@ -247,9 +222,12 @@ def _remote_create_container(cluster, task_definition, service_name, subnet, sec
     waiter.wait(cluster=cluster, services=[service_arn])
 
 def _remote_stop_container_args(args):
-    _remote_stop_container(ECS_DEFAULT_CLUSTER, DEFAULT_SERVICE_NAME)
+    remote_stop_container(ECS_DEFAULT_CLUSTER, DEFAULT_SERVICE_NAME)
 
-def _remote_stop_container(cluster, service_name):
+def remote_stop_container(cluster, service_name):
+    """
+    Stop a ECS task
+    """
     ecs_client = boto3.client('ecs', region_name=_get_region(cluster))
 
     resp = ecs_client.delete_service(cluster=cluster, service=service_name, force=True)
@@ -262,7 +240,34 @@ def _remote_stop_container(cluster, service_name):
 
     waiter.wait(cluster=cluster, services=[service_arn])
 
-def _remote_get_endpoint_str(cluster, service_name):
+def _remote_gc_services_container_args(args):
+    remote_gc_services_container(ECS_DEFAULT_CLUSTER)
+
+def remote_gc_services_container(cluster):
+    """
+    Delete all ECS services over then a given treshold.
+    """
+    ecs_client = boto3.client('ecs', region_name=_get_region(cluster))
+
+    services = ecs_client.list_services(cluster=cluster)
+
+    services_details = ecs_client.describe_services(cluster=cluster, services=services["serviceArns"])
+
+    not_expired_now = datetime.datetime.now().astimezone() - DEFAULT_GARBAGE_COLLECTION_THRESHOLD
+
+    for service in services_details["services"]:
+        created_at = service["createdAt"]
+
+        # Find the services that we created "too" long ago
+        if created_at < not_expired_now:
+            print("DELETING expired service %s which was created at %s." % (service["serviceName"], created_at))
+
+            remote_stop_container(cluster, service["serviceName"])
+
+def remote_get_endpoint_str(cluster, service_name):
+    """
+    Get an SSH connection string for the remote service
+    """
     ecs_client = boto3.client('ecs', region_name=_get_region(cluster))
     ec2_client = boto3.client('ec2', region_name=_get_region(cluster))
 
@@ -289,7 +294,7 @@ def _remote_get_endpoint_args(args):
     _remote_get_endpoint(ECS_DEFAULT_CLUSTER, DEFAULT_SERVICE_NAME)
 
 def _remote_get_endpoint(cluster, service_name):
-    endpoint = _remote_get_endpoint_str(cluster, service_name)
+    endpoint = remote_get_endpoint_str(cluster, service_name)
     print(endpoint)
 
 def _get_caller_identity(args):
@@ -302,15 +307,21 @@ def _run_e2e_test_args(args):
     _run_e2e_test(args.script, args.files)
 
 def _run_e2e_test(script, files):
+    """
+    Run a test end-to-end
 
-    _remote_create_container(ECS_DEFAULT_CLUSTER, ECS_DEFAULT_TASK_DEFINITION, DEFAULT_SERVICE_NAME, ECS_DEFAULT_SUBNET, ECS_DEFAULT_SECURITY_GROUP)
+    1. Start an ECS service
+    2. Copy the files over and run the test
+    3. Stop the ECS service
+    """
+    remote_create_container(ECS_DEFAULT_CLUSTER, ECS_DEFAULT_TASK_DEFINITION, DEFAULT_SERVICE_NAME, ECS_DEFAULT_SUBNET, ECS_DEFAULT_SECURITY_GROUP)
 
-    endpoint = _remote_get_endpoint_str(ECS_DEFAULT_CLUSTER, DEFAULT_SERVICE_NAME)
+    endpoint = remote_get_endpoint_str(ECS_DEFAULT_CLUSTER, DEFAULT_SERVICE_NAME)
 
     try:
         run_test(endpoint, script, files)
     finally:
-        _remote_stop_container(ECS_DEFAULT_CLUSTER, DEFAULT_SERVICE_NAME)
+        remote_stop_container(ECS_DEFAULT_CLUSTER, DEFAULT_SERVICE_NAME)
 
 def main() -> None:
     """Execute Main entry point."""
@@ -357,6 +368,12 @@ def main() -> None:
     # remote_stop_cmd.add_argument("--script", required=True, type=str, help="script to run")
     # remote_stop_cmd.add_argument("--files", type=str, nargs="*", help="Files to copy, each string must be a pair of src:dest joined by a colon")
     remote_stop_cmd.set_defaults(func=_remote_stop_container_args)
+
+    remote_gc_services_cmd = sub.add_parser('remote_gc_services', help='GC Remote Container')
+    # remote_gc_services_cmd.add_argument("--endpoint", required=True, type=str, help="User and Host and port, ie user@host:port")
+    # remote_gc_services_cmd.add_argument("--script", required=True, type=str, help="script to run")
+    # remote_gc_services_cmd.add_argument("--files", type=str, nargs="*", help="Files to copy, each string must be a pair of src:dest joined by a colon")
+    remote_gc_services_cmd.set_defaults(func=_remote_gc_services_container_args)
 
     get_caller_identity_cmd = sub.add_parser('get_caller_identity', help='Get the AWS IAM caller identity')
     get_caller_identity_cmd.set_defaults(func=_get_caller_identity)
