1616283f188a400b997724a76522aecf7f29ccaa
Mark Benvenuto <mark.benvenuto@mongodb.com>
2020-07-10 20:08:46 -0400

    printers

--- buildscripts/lldb/lldb_printers.py
+++ buildscripts/lldb/lldb_printers.py
@@ -45,6 +45,13 @@ def __lldb_init_module(debugger, *_args):
         "type synthetic add -x '^std::unique_ptr<.+>$' --python-class lldb_printers.UniquePtrPrinter"
     )
 
+    debugger.HandleCommand(
+        "type synthetic add -x '^absl::container_internal::flat_hash_set<.+>$' --python-class lldb_printers.AbslHashSetPrinter"
+    )
+    debugger.HandleCommand(
+        "type synthetic add -x '^absl::container_internal::raw_hash_map<.+>$' --python-class lldb_printers.AbslHashMapPrinter"
+    )
+
 
 #############################
 # Pretty Printer Defintions #
@@ -185,3 +192,127 @@ class OptionalPrinter:
             temp_type = self.valobj.GetType().GetTemplateArgumentType(0)
             storage = self.valobj.GetChildMemberWithName("m_storage")
             self.value = storage.Cast(temp_type)
+
+class AbslHashSetPrinter:
+    """Pretty printer for absl::container_internal::raw_hash_set."""
+
+    def __init__(self, valobj, *_args):
+        """Store the valobj and get the value of the hash_set."""
+        self.valobj = valobj
+        self.capacity = self.valobj.GetChildMemberWithName("capacity_").GetValueAsUnsigned()
+
+    def num_children(self):  # pylint: no-method-argument
+        """Match LLDB's expected API."""
+        return self.valobj.GetChildMemberWithName("size_").GetValueAsUnsigned()
+
+    def get_child_index(self, name):  # pylint: disable=no-self-use,no-method-argument
+        """Match LLDB's expected API."""
+        return None
+
+    def get_child_at_index(self, index):  # pylint: disable=no-self-use,no-method-argument
+        """Match LLDB's expected API."""
+        pos = 0
+        count = 0
+        ctrl = self.valobj.GetChildMemberWithName("ctrl_")
+        if index == 0 and ctrl.GetChildAtIndex(pos,False,True).GetValueAsSigned() > 0:
+            pos = 1
+        else:
+            while count <= index and pos <= self.capacity:
+                slot = ctrl.GetChildAtIndex(pos,False,True).GetValueAsSigned()
+                if slot >= 0:
+                    count += 1
+
+                pos += 1
+
+        o = self.valobj.GetChildMemberWithName("slots_").GetChildAtIndex(pos - 1,False,True)
+        print("OOO: %s - %s" % (index, pos));
+        return o.Dereference()
+
+
+    def has_children():  # pylint: disable=no-self-use,no-method-argument
+        """Match LLDB's expected API."""
+        return True
+
+
+# https://github.com/WebKit/webkit/blob/master/Tools/lldb/lldb_webkit.py
+class AbslHashMapPrinter:
+    """Pretty printer for absl::container_internal::raw_hash_map."""
+
+    def __init__(self, valobj, internal_dict):
+        """Store the valobj and get the value of the hash_map."""
+        print("Constructor: %s" % (valobj.GetID()))
+        self.valobj = valobj
+        print("Loc1 %s" % (self.valobj.GetLocation()))
+        print("Loc2 %s" % (self.valobj.GetChildMemberWithName("slots_").GetLocation()))
+        print("Loc3 %s" % (self.valobj.GetChildMemberWithName("slots_").GetChildAtIndex(0).GetLocation()))
+        print("Loc4 %s" % (self.valobj.GetChildMemberWithName("slots_").GetChildAtIndex(0).GetChildMemberWithName("first").GetLocation()))
+        a = self.valobj.GetChildMemberWithName("slots_").GetChildAtIndex(0).GetChildMemberWithName("first").GetSummary()
+        print("FOO %s" % (len(a)))
+        print("FOO %s" % (a))
+        print("FOO %s  -" % (''.join(hex(ord(c)) for c in a)))
+        self.capacity = self.valobj.GetChildMemberWithName("capacity_").GetValueAsUnsigned()
+        self.data_type = self.valobj.GetChildMemberWithName("slots_").GetType().GetPointeeType()
+        self.data_size = self.data_type.GetByteSize()
+
+    def num_children(self):  # pylint: no-method-argument
+        """Match LLDB's expected API."""
+        #return self.valobj.GetChildMemberWithName("size_").GetValueAsUnsigned()
+        return 1
+
+    def get_child_index(self, name):  # pylint: disable=no-self-use,no-method-argument
+        """Match LLDB's expected API."""
+        return None
+
+    def get_child_at_index(self, index):  # pylint: disable=no-self-use,no-method-argument
+        """Match LLDB's expected API."""
+        pos = 0
+        count = 0
+        ctrl = self.valobj.GetChildMemberWithName("ctrl_")
+        if index == 0 and ctrl.GetChildAtIndex(pos,False,True).GetValueAsSigned() > 0:
+            pos = 1
+        else:
+            while count <= index and pos <= self.capacity:
+                slot = ctrl.GetChildAtIndex(pos,False,True).GetValueAsSigned()
+                if slot >= 0:
+                    count += 1
+
+                pos += 1
+
+        value = self.valobj.GetChildMemberWithName("slots_").GetChildAtIndex(pos - 1,False,False)
+
+        print("pos: %s" % (pos))
+        print("t: %s" %( self.data_type))
+        print("s: %s" %( value.IsSynthetic()))
+        print("s: %s" %( value.IsDynamic()))
+        print("s: %s" %( value.IsValid()))
+        print("s: %s" %( value.GetID()))
+        print("v: %s" %( value.GetChildMemberWithName("first")))
+        print("v: %s" %( value.GetChildMemberWithName("first").GetName()))
+        print("v: %s" %( value.GetChildMemberWithName("first").GetTypeName()))
+        print("l: %s" %( value.GetChildMemberWithName("first").GetLocation()))
+        print("l: %s" %( value.GetChildMemberWithName("first").AddressOf()))
+        print("v: %s" %( value.GetChildMemberWithName("first").GetValue()))
+        print("d: %s"  %( value.GetChildMemberWithName("first").GetData()))
+        print("d: %s"  %( value.GetChildMemberWithName("first").GetSummary()))
+
+        print("v: %s" %( value.GetChildMemberWithName("second")))
+        print("v: %s" %( value.GetChildMemberWithName("second").GetName()))
+        print("v: %s" %( value.GetChildMemberWithName("second").GetTypeName()))
+        print("l: %s" %( value.GetChildMemberWithName("second").GetLocation()))
+        print("l: %s" %( value.GetChildMemberWithName("second").AddressOf()))
+        print("v: %s" %( value.GetChildMemberWithName("second").GetValue()))
+        print("d: %s"  %( value.GetChildMemberWithName("second").GetData()))
+        print("d: %s"  %( value.GetChildMemberWithName("second").GetSummary()))
+
+        o = self.valobj.GetChildMemberWithName("slots_").CreateChildAtOffset("[" + str(value.GetChildMemberWithName("key").GetValue()) + "]", self.data_size * (pos - 1), self.data_type)
+        print("OOO: %s - %s" % (index, pos));
+        return o
+
+
+    def has_children():  # pylint: disable=no-self-use,no-method-argument
+        """Match LLDB's expected API."""
+        return True
+
+    def update():
+        pass
+
