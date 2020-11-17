d4dde0c7a03c00e26d35c91b21c8ce61aa1180a2
Mark Benvenuto <mark.benvenuto@mongodb.com>
2020-11-17 14:47:08 -0500

    snap

--- jstests/ssl/ssl_intermediate_ca.js
+++ jstests/ssl/ssl_intermediate_ca.js
@@ -19,23 +19,52 @@ function runTest(inbound, outbound) {
         sslClusterCAFile: inbound,
     });
     assert(mongod);
+    assert.commandWorked(mongod.getDB('admin').runCommand('serverStatus'));
     assert.eq(mongod.getDB('admin').system.users.find({}).toArray(), []);
     MongoRunner.stopMongod(mongod);
 }
 
-// Normal mode, we have a valid CA being presented for outbound and inbound.
-runTest(VALID_CA, VALID_CA);
+// // Normal mode, we have a valid CA being presented for outbound and inbound.
+// runTest(VALID_CA, VALID_CA);
 
-// Alternate CA mode, only the inbound CA is valid.
-runTest(VALID_CA, INVALID_CA);
+// // Alternate CA mode, only the inbound CA is valid.
+// runTest(VALID_CA, INVALID_CA);
 
-// Validate we can make a connection from the shell with the intermediate certs
+// // Validate we can make a connection from the shell with the intermediate certs
+// {
+//     const mongod = MongoRunner.runMongod({
+//         sslMode: 'requireSSL',
+//         sslAllowConnectionsWithoutCertificates: '',
+//         sslPEMKeyFile: 'jstests/libs/server.pem',
+//         sslCAFile: VALID_CA,
+//     });
+//     assert(mongod);
+//     assert.eq(mongod.getDB('admin').system.users.find({}).toArray(), []);
+
+//     const smoke = runMongoProgram("mongo",
+//                                   "--host",
+//                                   "localhost",
+//                                   "--port",
+//                                   mongod.port,
+//                                   "--ssl",
+//                                   "--sslCAFile",
+//                                   VALID_CA,
+//                                   "--sslPEMKeyFile",
+//                                   "jstests/libs/server-intermediate-ca.pem",
+//                                   "--eval",
+//                                   "1;");
+//     assert.eq(smoke, 0, "Could not connect with intermediate certificate");
+
+//     MongoRunner.stopMongod(mongod);
+// }
+
+// Validate we can make a with intermediate certs in ca file instead of key file
 {
     const mongod = MongoRunner.runMongod({
         sslMode: 'requireSSL',
         sslAllowConnectionsWithoutCertificates: '',
-        sslPEMKeyFile: 'jstests/libs/server.pem',
-        sslCAFile: VALID_CA,
+        sslPEMKeyFile: 'jstests/libs/server-intermediate-leaf.pem',
+        sslCAFile: 'jstests/libs/intermediate-ca-chain.pem',
     });
     assert(mongod);
     assert.eq(mongod.getDB('admin').system.users.find({}).toArray(), []);
@@ -49,7 +78,7 @@ runTest(VALID_CA, INVALID_CA);
                                   "--sslCAFile",
                                   VALID_CA,
                                   "--sslPEMKeyFile",
-                                  "jstests/libs/server-intermediate-ca.pem",
+                                  "jstests/libs/client.pem",
                                   "--eval",
                                   "1;");
     assert.eq(smoke, 0, "Could not connect with intermediate certificate");
